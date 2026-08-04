d8a184205336fd7d9cf8a6e5386b0cccecd8e99b
RGUTIERREZ <gutierrezrussbell@gmail.com>
2026-08-04 04:43:17 -0500

    Update 2026-08-04

--- app/src/main/java/com/upd/kvupd/data/remote/ApiRetrofitInstance.kt
+++ app/src/main/java/com/upd/kvupd/data/remote/ApiRetrofitInstance.kt
@@ -2,13 +2,27 @@ package com.upd.kvupd.data.remote
 
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
 
 object ApiRetrofitInstance {
-    fun createAPI(ip: String, moshi: MoshiConverterFactory): ApiService {
+
+    private const val API_TIMEOUT_SECONDS = 120L
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .callTimeout(API_TIMEOUT_SECONDS, TimeUnit.SECONDS)
+        .build()
+
+    fun createAPI(
+        ip: String,
+        moshi: MoshiConverterFactory
+    ): ApiService {
         val retrofit = Retrofit.Builder()
             .baseUrl("http://$ip/api/")
+            .client(okHttpClient)
             .addConverterFactory(moshi)
             .build()
+
         return retrofit.create(ApiService::class.java)
     }
 }
